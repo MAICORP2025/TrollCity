7ab680155e883f57a1b26478b0ba2c343ab6b113
kaintowns83 <kaintowns83@gmail.com>
2025-12-11 01:50:54 -0700

    Add service worker skipWaiting and cache clearing, remove Process Weekly TrollTract button from admin dashboard

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 // Service Worker for PWA
+self.skipWaiting();
 const CACHE_NAME = 'trollcity-admin-v1'
 const urlsToCache = [
   '/admin-mobile',
@@ -9,6 +10,15 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
   )
 })
 
