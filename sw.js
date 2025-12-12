898ec73b5f108a424cf518ec668fab9cac5b89f3
kaintowns83 <kaintowns83@gmail.com>
2025-12-12 02:47:59 -0700

    Force service worker takeover on update

--- sw.js
+++ sw.js
@@ -2,9 +2,12 @@ self.skipWaiting();
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => caches.delete(key)))
-    )
+    Promise.all([
+      caches.keys().then((keys) =>
+        Promise.all(keys.map((key) => caches.delete(key)))
+      ),
+      self.clients.claim()
+    ])
   );
 });
 
