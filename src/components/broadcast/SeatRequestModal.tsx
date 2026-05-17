31bca1e3a88a13da55ee691d6db9f8335a1bccc1
kainm <kainm@users.noreply.github.com>
2026-05-17 14:33:31 -0600

    seat

--- src/components/broadcast/SeatRequestModal.tsx
+++ src/components/broadcast/SeatRequestModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from '../ui/Button';
+import { Button } from '../ui/button';
 import { toast } from 'sonner';
 
 interface SeatRequestModalProps {
